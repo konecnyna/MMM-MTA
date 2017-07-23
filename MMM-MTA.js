a61e35cd8276cd447a1bc26123917149cc377f1e
Nick Konecny <nkonecny@etsy.com>
2017-07-23 12:36:42 -0400

    Added mono-chrome style

--- MMM-MTA.js
+++ MMM-MTA.js
@@ -25,27 +25,52 @@ Module.register("MMM-MTA",{
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		
+
+		var table = document.createElement("table");
+		table.className = "mta";
+
+		var delaysText;
+
 		this.linesData.map(line => {
-			var container = document.createElement("div");		
+			var row = document.createElement("tr");
+			table.appendChild(row);
 
-			var text = document.createElement("div");		
-			text.innerHTML = line.name[0] + ": " + line.status;		
-			if (line.status[0] === "DELAYS") {
-				container.className = "animate-flicker";				
-				text.style = "color: " + line.color;			
-			}
-				
-			container.appendChild(text);
-			wrapper.appendChild(container);    		
+			var circles = "";
+			line.name[0].split("").map(lineName => {
+				circles += "<div class='circle'>" + lineName + "</div>";
+			});
+			
+			var col1 = document.createElement("td");		
+			var col2 = document.createElement("td");
 
+			col1.style = "float:left;";
+			col1.innerHTML = circles;
+			col2.innerHTML = line.status;
+
+			if (line.status[0] === "PLANNED WORK") {
+				row.className = "animate-flicker";				
+				row.style = "color: red";
+				delaysText = line.text[0];				
+			}	
+			row.appendChild(col1);
+			row.appendChild(col2);					
 		});
 
+		wrapper.appendChild(table);
+
 		if (this.linesData && this.linesData.length > 0) {
-			console.log(this.linesData[0]);
 			var lastUpdated = document.createElement("div");			
 			lastUpdated.innerHTML = "Last updated: " + this.linesData[0].Time;
 			wrapper.appendChild(lastUpdated);			
 		}
+		
+		if (delaysText) {
+			var marquee = document.createElement("marquee");
+			marquee.style = "white-space: nowrap;"
+			marquee.innerText = delaysText;
+			//wrapper.appendChild(marquee);
+		}
+		
 		
 		return wrapper;
 	},
